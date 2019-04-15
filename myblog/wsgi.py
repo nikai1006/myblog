cb0c9c2d7fc2b3a8c82a545ace3a24ca407b8e6d
nikai <keni1006@163.com>
2019-04-16 00:21:30 +0800

    https://docs.djangoproject.com/en/2.2/

--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -5,6 +5,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
+
+WSGI(Python Web Server Gateway Interface)
+中文名: Python服务网关接口
+Python应用与Web服务器之间的接口
 """
 
 import os
