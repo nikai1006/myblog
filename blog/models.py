2c5578cdc0e4ab6d79df4ea2c530ba9cd856d50d
nikai <keni1006@163.com>
2019-04-22 01:52:06 +0800

    urls

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
 # 类似java的DTO
+
+class Article(models.Model):
+    title = models.CharField(max_length=32, default='title')
+    content = models.TextField(null=True)
