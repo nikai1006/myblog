0bfb0fb08107ce32d9aaab38f13707fc27008210
nikai <keni1006@163.com>
2019-04-26 01:12:40 +0800

    admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import Article
 
+
 # Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content')
+
 
 admin.site.register(Article)
