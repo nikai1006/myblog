b27f7c7a595555b99c0aed3730c1763e77fd28ab
nikai <keni1006@163.com>
2019-05-29 00:43:27 +0800

    add nginx.py

--- nginx.py
+++ nginx.py
@@ -4,8 +4,23 @@ Nginx控制程序
 
 import os
 import commands
+import paramiko
+import time
 
 WORK_PATH = os.getcwd()
+ssh = paramiko.SSHClient()
+sc = ssh.invoke_shell()
+root_pwd = 'nikai'
+
+
+def exe_cmd(cmd, t=0.1):
+    sc.send(cmd)
+    sc.send("\n")
+    time.sleep(t)
+    resp = sc.recv(9999).decode("utf8")
+    # print "cmd='%s',echo='%s'\n"%(cmd,resp)
+    return resp
+
 
 if __name__ == '__main__':
     print(WORK_PATH)
@@ -14,3 +29,7 @@ if __name__ == '__main__':
     print("---------------------------------------")
     (status, output) = commands.getstatusoutput('java -version')
     print(status, output)
+    # 切换root账号
+    resp = exe_cmd("su root", t=1)
+    if resp.endswith(u"密码："):
+        resp = exe_cmd(root_pwd)
