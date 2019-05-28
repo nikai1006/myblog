21b77ec54e6bd04e971da928ba72b93e0db9003a
nikai <keni1006@163.com>
2019-05-29 00:24:23 +0800

    add nginx.py

---
+++ nginx.py
@@ -0,0 +1,16 @@
+"""
+Nginx控制程序
+"""
+
+import os
+import commands
+
+WORK_PATH = os.getcwd()
+
+if __name__ == '__main__':
+    print(WORK_PATH)
+    output = os.popen('java -version')
+    print(output.read())
+    print("---------------------------------------")
+    (status, output) = commands.getstatusoutput('cat /proc/cpuinfo')
+    print(status, output)
