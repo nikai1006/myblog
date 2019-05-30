e795a3e16a216b692c64598ecbf130c80c5ea654
尼凯 <nikai.ni@klook.com>
2019-05-30 09:30:40 +0800

    add nginx

--- nginx.py
+++ nginx.py
@@ -3,33 +3,45 @@ Nginx控制程序
 """
 
 import os
-import paramiko
-import time
+import subprocess
+
+# import paramiko
+# import time
 # import commands
 
 WORK_PATH = os.getcwd()
-ssh = paramiko.SSHClient()
-sc = ssh.invoke_shell()
-root_pwd = 'nikai'
+# ssh = paramiko.SSHClient()
+# sc = ssh.invoke_shell()
+# root_pwd = 'nikai'
 
 
-def exe_cmd(cmd, t=0.1):
-    sc.send(cmd)
-    sc.send("\n")
-    time.sleep(t)
-    resp = sc.recv(9999).decode("utf8")
-    # print "cmd='%s',echo='%s'\n"%(cmd,resp)
-    return resp
+# def exe_cmd(cmd, t=0.1):
+#     sc.send(cmd)
+#     sc.send("\n")
+#     time.sleep(t)
+#     resp = sc.recv(9999).decode("utf8")
+#     print "cmd='%s',echo='%s'\n"%(cmd,resp)
+#     return resp
 
 
 if __name__ == '__main__':
     print(WORK_PATH)
-    output = os.popen('sudo nginx -t')
-    print(output.read())
+    output = os.popen('nginx -t')
+    output_read = output.read()
+    # result = str(output_read)
+    # print(output_read)
+    # print(result)
+
+    # print(output_read)
+    print("==========================")
+    # validation = output_read.split(' ')[-1]
+    # print(validation)
+    output = os.popen('nginx -s reload')
+    # print(output.read())
     print("---------------------------------------")
     # (status, output) = commands.getstatusoutput('java -version')
     # print(status, output)
     # 切换root账号
-    resp = exe_cmd("su root", t=1)
-    if resp.endswith(u"密码："):
-        resp = exe_cmd(root_pwd)
+    # resp = exe_cmd("su root", t=100)
+    # if resp.endswith(u"密码："):
+    #     resp = exe_cmd(root_pwd)
