1a6bc78cb343268a0fb9a38a6f4508b6e15dc70c
nikai <keni1006@163.com>
2019-06-23 19:15:15 +0800

    supervisor

--- myblog/settings/docker.py
+++ myblog/settings/docker.py
@@ -1,5 +1,10 @@
 from .base import *
 
+BROKER_BACKEND = 'redis'
+BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/2"
+
+DEBUG = False
 
 DATABASES = {
     'default': {
@@ -7,7 +12,7 @@ DATABASES = {
         'NAME': 'myblog',
         'USER': 'test',
         'PASSWORD': '123456',
-        'HOST': 'dockerdb',
+        'HOST': 'mysql',
         'PORT': '3306',
     }
 }
